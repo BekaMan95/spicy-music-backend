2d352e4bae8600dc46f068908262589506c1908f
BekaMan95 <bsahle95@gmail.com>
2025-10-24 23:27:00 +0300

    custom header handling

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ app.use(helmet());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", JSON.stringify(process.env.CORS_ORIGIN));
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
