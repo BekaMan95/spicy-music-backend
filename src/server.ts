6ee3247de198ee405b78337d15f0a01dfcdd04da
BekaMan95 <bsahle95@gmail.com>
2025-10-25 08:27:26 +0300

    Access-Control-Allow-Headers removed

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ app.use(helmet());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
